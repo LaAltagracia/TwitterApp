73d84ee27ba89e4db8924d4bfac0b24e5de58d70
Chari <74219169+LaMuchachaT@users.noreply.github.com>
2022-09-27 22:33:08 -0400

    added twitter app

---
+++ Twitter/tweetCel.swift
@@ -0,0 +1,24 @@
+//
+//  tweetCel.swift
+//  Twitter
+//
+//  Created by Chari Altagracia on 9/26/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCel: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
